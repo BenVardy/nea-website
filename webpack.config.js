6488a4f4e9dc40b2a2aa77da8f0f1a3a4e172f1c
Ben Vardy <ben@vardyfamily.co.uk>
2019-08-26 10:42:16 +0100

    Configure configs

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: './src/app.ts',
+    module: {
+        rules : [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: /src/
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name].[ext]'
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contenBase: path.resolve(__dirname, 'build/public'),
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001'
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build/public')
+    }
+}
