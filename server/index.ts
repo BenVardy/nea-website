a2aa35e0e7c4315a4a0dc9577b79668351b9ff8f
Ben Vardy <ben@vardyfamily.co.uk>
2019-08-26 11:14:49 +0100

    Add basic server setup

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+
+let app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV !== 'development') {
+    console.log('Serving static files');
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
