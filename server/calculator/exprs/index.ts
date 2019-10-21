3bddc88bfaa9ee8ddb692058f85ffd38286f6621
Ben Vardy <ben@vardyfamily.co.uk>
2019-10-21 08:17:16 +0100

    Change plus to addition

--- server/calculator/exprs/index.ts
+++ server/calculator/exprs/index.ts
@@ -1,6 +1,6 @@
 export { default as GetDet } from './getDet';
 export { default as Eigen } from './eigen';
 export { default as Exponent } from './exponent';
-export { default as Plus } from './addition';
+export { default as Addition } from './addition';
 export { default as Multiplication } from './multiplication';
 export { default as Division } from './division';
