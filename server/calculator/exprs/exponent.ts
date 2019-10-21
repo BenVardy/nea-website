1e0c5321a6779648fb9ecb82fc54113ccae813f3
Ben Vardy <ben@vardyfamily.co.uk>
2019-10-21 08:16:17 +0100

    Add basic matrix powers

--- server/calculator/exprs/exponent.ts
+++ server/calculator/exprs/exponent.ts
@@ -14,17 +14,32 @@ export default class Exponent implements IExpr {
 
         if (param2.type !== 'no') throw new Error('Must have an exponent of a number');
 
+        let exponent: number = (param2.data as number);
+
         if (param1.type === 'no') {
             return [{
                 type: 'no',
-                data: Math.pow(param1.data as number, param2.data as number)
+                data: Math.pow(param1.data as number, exponent)
             }];
         } else if (param1.type === 'matrix') {
-            throw new Error('No matrix powers yet');
+            let matrix: Matrix = param1.data as Matrix;
+            let finalMatrix: Matrix;
+
+            if (exponent === -1) {
+                finalMatrix = matrix.invert();
+            } else if (exponent > 0) {
+                finalMatrix = matrix.clone();
+                for (let i = 0; i < (param2.data as number); i++) {
+                    finalMatrix = finalMatrix.multiply(matrix);
+                }
+            } else {
+                throw new Error('Invalid exponent');
+            }
+
             return [{
                 type: 'matrix',
                 // Put in to the power
-                data: (param1.data as Matrix)
+                data: finalMatrix
             }];
         } else {
             throw new Error('Cannot take powers of vectors');
