9c6398684035e108a1e433b6ffc079a44ad6669a
Ben Vardy <ben@vardyfamily.co.uk>
2019-10-21 08:15:51 +0100

    Update division to be more like multiplication

--- server/calculator/exprs/division.ts
+++ server/calculator/exprs/division.ts
@@ -11,24 +11,17 @@ export default class Division implements IExpr {
         if (params.length !== 2) throw new Error('Must have 2 params');
 
         let [param1, param2] = params;
+
         if (param1.type === 'no' && param2.type === 'no') {
             return [{
                 type: 'no',
                 data: (param1.data as number) / (param2.data as number)
             }];
         } else if (param2.type === 'no') {
-            if (param1.type === 'matrix') {
-                return [{
-                    type: 'matrix',
-                    data: (param1.data as Matrix).multiply(1 / (param2.data as number))
-                }];
-            } else {
-                // param1 must be a vector
-                return [{
-                    type: 'vector',
-                    data: (param1.data as Vector).multiply(1 / (param2.data as number))
-                }];
-            }
+            return [{
+                type: param1.type,
+                data: (param1.data as Matrix|Vector).multiply(1 / (param2.data as number))
+            }];
         }
 
         throw new Error('Invalid division');
